46458fd0d72982780d06f0cb49d5d6a3cb79afe8
Alcione de Lucca Júnior <junior.dluk@gmail.com>
2018-10-30 14:03:52 -0300

    docs(Server): Server and README.md updates

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -248,7 +248,7 @@ module.exports = {
       {
         test: /\.(js|jsx)$/,
         enforce: 'pre',
-        exclude: /node_modules/,
+        exclude: [/node_modules/, /oauth-server/],
         loader: "eslint-loader",
         options: {
           failOnError: true,
