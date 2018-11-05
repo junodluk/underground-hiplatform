064968e098b9eda3b08babeedaa81d902c2641fc
Alcione de Lucca Júnior (Juno) <junior.dluk@gmail.com>
2018-11-05 07:38:40 -0200

    chore(Router): Many changes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,59 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './app.scss';
-import { Button } from 'components/button';
-import { executeLogin } from 'api';
+import { Toolbar } from 'react-md';
+import { PureSearch } from 'components/search';
+// import { executeLogin } from 'api';
 
 class App extends Component {
 
+  onSearch = (search) => {
+    this.setState({ search });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/app.js</code> and save to reload.
-          </p>
-          <Button onClick={() => executeLogin()} />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/app.js</code> and save to reload.
+      //     </p>
+      //     <Button onClick={() => executeLogin()} />
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
+      <Fragment>
+        <Toolbar
+          colored
+          fixed
+          actions={<PureSearch onSearch={this.onSearch} />}
+          title="react-app"
+        />
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/app.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        </div>
+      </Fragment>
     );
   }
 }
